3e1f4d0dc02158e324937027b05e41885a48a5c1
Adel <adeleslami0077@yahoo.com>
2025-01-21 14:41:43 +0330

    Initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,33 @@
 import type { Config } from "tailwindcss";
 
-const config: Config = {
+export default {
   content: [
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/components/button.js",
   ],
   theme: {
     extend: {
+      
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        
       },
+      fontFamily: {
+        sans: ['var(--font-morabba)']
+      },
+      margin: {
+        '5px': '5px',
+      }
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light"], // true: all themes | false: only light + dark | array: specific themes like this ["light", "dark", "cupcake"]
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    rtl: true, // rotate style direction from left-to-right to right-to-left. You also need to add dir="rtl" to your html tag and install `tailwindcss-flip` plugin for Tailwind CSS.
+  },
+} satisfies Config;
